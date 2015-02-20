2593387a16f1caf31c4dfad3f795743cf84f87ad
Vit Brunner <vit.brunner@gmail.com>
2015-02-20 15:27:59 +0100

    First draft feedback model

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=200)
+
+class Questionnaire(models.Model):
+    name = models.CharField(max_length=200)
+
+class Question(models.Model):
+    questionnaire = models.ForeignKey(Questionnaire)
+    question_type = models.CharField(max_length=1, choices=(
+        ('t', 'text'),
+        ('c', 'choice'),
+        ('m', 'multichoice'),
+    ))
+    mandatory = models.BooleanField()
+
+class Feedback(models.Model):
+    creator = models.ForeignKey(User)
+    answerer = models.ForeignKey(User)
+    questionnaire = models.ForeignKey(Questionnaire)
+
+class Answer(models.Model):
+    feedback = models.ForeignKey(Feedback)
+    answer = models.CharField(max_length=200)
