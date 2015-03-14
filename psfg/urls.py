4429ee4c94787b7a55f39c1c759a03138e709bb9
Vit Brunner <vit.brunner@gmail.com>
2015-03-14 15:52:27 +0100

    Add homepage

--- psfg/urls.py
+++ psfg/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'psfg.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'homepage.views.index'),
     url(r'^admin/', include(admin.site.urls)),
 )
