a2edefc97da317b582cb214bdee4af8812cb1070
Vit Brunner <vit.brunner@gmail.com>
2015-03-14 20:07:53 +0100

    Redirect /mail

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import redirect, render_to_response
 
 def index(request):
     return render_to_response('index.html')
+
+def mail(request):
+    return redirect('http://eepurl.com/bcn2t5', True)
--- psfg/urls.py
+++ psfg/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'homepage.views.index'),
+    url(r'^mail$', 'homepage.views.mail'),
     url(r'^admin/', include(admin.site.urls)),
 )
