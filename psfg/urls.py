f4586b2c597f1e37ba87e92fa49ab8b3bad9cf8e
michalparkola <michal.parkola@gmail.com>
2015-03-21 17:56:25 +0100

    Create basic feedback form

--- psfg/urls.py
+++ psfg/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('',
     url(r'^$', 'homepage.views.index'),
     url(r'^labs$', 'homepage.views.labs'),
     url(r'^mail$', 'homepage.views.mail'),
+    url(r'^feedback', 'feedback.views.index'),
     url(r'^admin/', include(admin.site.urls)),
 )
