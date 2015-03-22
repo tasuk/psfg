2c12dc70bc4a22815cc91e19053d746069733f6b
michalparkola <michal.parkola@gmail.com>
2015-03-22 12:25:35 +0100

    Feedback urls in separate file

--- psfg/urls.py
+++ psfg/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     url(r'^$', 'homepage.views.index'),
     url(r'^labs$', 'homepage.views.labs'),
     url(r'^mail$', 'homepage.views.mail'),
-    url(r'^feedback', 'feedback.views.index'),
+    url(r'^feedback/', include('feedback.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
