a2288f3965e4d2e50b0451e7db97212ccf51e3a6
Vit Brunner <vit.brunner@gmail.com>
2015-03-25 22:25:15 +0100

    Add flatpages

--- psfg/urls.py
+++ psfg/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^$', 'homepage.views.index'),
     url(r'^labs$', 'homepage.views.labs'),
     url(r'^mail$', 'homepage.views.mail'),
+    url(r'^articles/', include('django.contrib.flatpages.urls')),
     url(r'^feedback/', include('feedback.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
