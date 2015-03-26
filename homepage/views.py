e8dd54e5a55e2f097bd7a5a2c74b69e162b6e0f3
Vit Brunner <vit.brunner@gmail.com>
2015-03-26 18:13:25 +0100

    Fix non-cms pages

--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import redirect, render_to_response
+from django.template import RequestContext
 
 def index(request):
     return redirect('/labs')
 
 def labs(request):
-    return render_to_response('index.html')
+    return render_to_response('index.html', RequestContext(request))
 
 def mail(request):
     return redirect('http://eepurl.com/bcn2t5', True)
