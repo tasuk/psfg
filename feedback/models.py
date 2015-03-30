65f2b791dd1bce89ec7cf3c48ab2ffd4fbdc1b58
Vit Brunner <vit.brunner@gmail.com>
2015-03-31 00:11:36 +0200

    Simplify feedback models, create admin

--- feedback/models.py
+++ feedback/models.py
@@ -1,35 +1,22 @@
 from django.db import models
 
-class User(models.Model):
-    email = models.CharField(max_length=200)
+# This is an utter abomination, but hey, we're prototyping!
 
 class Questionnaire(models.Model):
-    name = models.CharField(max_length=200)
+    public_id = models.CharField(max_length=16, unique=True)
+    name = models.CharField(max_length=100, blank=True)
+    asker_email = models.CharField(max_length=200)
+    asker_name = models.CharField(max_length=200)
 
-class Question(models.Model):
-    TEXT = 't'
-    CHOICE = 'c'
-
-    questionnaire = models.ForeignKey(Questionnaire)
-    label = models.CharField(max_length=200)
-    question_type = models.CharField(max_length=1, choices=(
-        (TEXT, 'text'),
-        (CHOICE, 'choice'),
-    ))
-    mandatory = models.BooleanField(default=False)
-    order = models.IntegerField()
-
-class Option(models.Model):
-    question = models.ForeignKey(Question)
-    name = models.CharField(max_length=200)
-    label = models.CharField(max_length=200)
-    order = models.IntegerField()
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.public_id)
 
 class Feedback(models.Model):
     questionnaire = models.ForeignKey(Questionnaire)
-    creator = models.ForeignKey(User, related_name='feedback_creator')
-    answerer = models.ForeignKey(User, related_name='feedback_answerer', blank=True, null=True)
+    giver_name = models.CharField(max_length=200, blank=True)
+    workagain = models.IntegerField()
+    didenjoy = models.TextField(blank=True)
+    didnotenjoy = models.TextField(blank=True)
 
-class Answer(models.Model):
-    feedback = models.ForeignKey(Feedback)
-    answer = models.CharField(max_length=200)
+    def __str__(self):
+        return "For %s (%s); %s (%i)" % (self.questionnaire.name, self.questionnaire.public_id, self.giver_name, self.id)
