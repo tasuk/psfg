5203d18abec3ffc61a3a8403034c251bb1bc4e5e
Vit Brunner <vit.brunner@gmail.com>
2015-03-31 00:09:09 +0200

    More djangoish urls

--- content/urls.py
+++ content/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^mail$', views.mail),
+    url(r'^mail/$', views.mail),
+    url(r'^meetup/$', views.meetup),
     url(r'^', include('cms.urls')),
 )
--- content/views.py
+++ content/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import redirect, render_to_response
 from django.template import RequestContext
 
 def mail(request):
-    return redirect('http://eepurl.com/bcn2t5', True)
+    return redirect('http://eepurl.com/bcn2t5')
+
+def meetup(request):
+    return redirect('http://www.meetup.com/People-Skills-for-Geeks')
