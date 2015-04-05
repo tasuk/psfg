d485fa1fced4eddba288bd7fb048c12083590c17
Vit Brunner <vit.brunner@gmail.com>
2015-04-05 13:13:54 +0200

    Pass questionnaire to give template

--- feedback/views.py
+++ feedback/views.py
@@ -25,9 +25,11 @@ def ask(request):
     })
 
 def give(request, public_id):
+    questionnaire = Questionnaire.objects.get(public_id=public_id)
+
     if request.method == 'POST':
         feedback = Feedback()
-        feedback.questionnaire = Questionnaire.objects.get(public_id=public_id)
+        feedback.questionnaire = questionnaire
         feedback.workagain = request.POST['workagain']
         feedback.didenjoy = request.POST['didenjoy']
         feedback.didnotenjoy = request.POST['didnotenjoy']
@@ -36,6 +38,7 @@ def give(request, public_id):
         return redirect('thanks')
 
     return render(request, 'give.html', {
+        'questionnaire': questionnaire,
         'workagain_options': Feedback.workagain_options,
     })
 
